c806b5479808f9f1ee4339a81bd42e7e860470d1
Matthew Liu <mattliurw@gmail.com>
2023-10-23 18:47:05 +1100

    init tute07

---
+++ tute07/src/calculator/composite/Multiplication.java
@@ -0,0 +1,15 @@
+package calculator.composite;
+
+// public class Multiplication implements Expression {
+//     private Expression lhs;
+//     private Expression rhs;
+
+//     public Multiplication(Expression lhs, Expression rhs) {
+//         this.lhs = lhs;
+//         this.rhs = rhs;
+//     }
+
+//     public double compute() {
+//         return lhs.compute() * rhs.compute();
+//     }
+// }
