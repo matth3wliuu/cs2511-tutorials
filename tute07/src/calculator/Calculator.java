5745090396a14c5e4bcf5b7d63091bbab8962964
Matthew Liu <mattliurw@gmail.com>
2023-10-23 18:47:28 +1100

    init tute07

---
+++ tute07/src/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+public class Calculator {
+
+
+    public static void main(String[] args) {
+
+        // exp1 = 42
+
+
+        // exp2 = ((1 + 2) - ((3 / 4) * 5))
+
+    }
+}
