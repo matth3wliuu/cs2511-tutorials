4345312a99c1df1f2a713a3281bfde038a369ea3
Matthew Liu <mattliurw@gmail.com>
2024-02-12 14:48:46 +1100

    init tute01

--- tute01/src/example/Sum.java
+++ tute01/src/example/Sum.java
@@ -1,5 +1,13 @@
 package example;
 
+import java.util.Scanner;
+
+/*
+ * Write a program that uses the `Scanner` class
+ * which reads in a line of numbers separated by spaces
+ * from stdin, and sums them.
+*/
+
 public class Sum {
     public static void main(String[] args) {
     }
